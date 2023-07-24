679ab2b535553086058520b06ee7f45ca982c8ba
danielpdaniel <artofjquinn@gmail.com>
2023-07-24 15:28:04 -0400

    Convert camera image to grayscale and add blur effect

--- ball_tracking.py
+++ ball_tracking.py
@@ -7,8 +7,13 @@ while True:
     ret, frame = video.read()
     if not ret: break
 
-    cv2.imshow("frame", frame)
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    blur_frame = cv2.GaussianBlur(gray_frame, (19,19), 0)
+
+    cv2.imshow("frame", blur_frame)
+    # cv2.imshow("frame", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'): break
+    # cv2.waitKey(1)
 
 video.release()
 cv2.destroyAllWindows()
