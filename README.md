160dc48713db7a0d980a02af03256ad7b7e3fb74
danielpdaniel <artofjquinn@gmail.com>
2023-07-24 18:50:12 -0400

    Update README with description and credits

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ball-tracking
+Simply ball tracking program using cv2 and numpy to get my feet wet with Python and with image rendering. Run program to start, press q to quit.
+
+# credits
+Mostly followed along with [this tutorial](https://www.youtube.com/watch?v=RaCwLrKuS1w&t=53s) by CodeSavant on YouTube
--- ball_tracking.py
+++ ball_tracking.py
@@ -1,7 +1,9 @@
 import cv2
-# import numpy
+import numpy
 
 video = cv2.VideoCapture(0)
+prevCircle = None
+distance = lambda x1,y1,x2,y2: (x1-x2)**2+(y1-y2)**2
 
 while True:
     ret, frame = video.read()
@@ -10,7 +12,24 @@ while True:
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     blur_frame = cv2.GaussianBlur(gray_frame, (19,19), 0)
 
-    cv2.imshow("frame", blur_frame)
+    circles = cv2.HoughCircles(blur_frame, cv2.HOUGH_GRADIENT, 1.2, 100, 
+                               param1=100, param2=30, minRadius=75, maxRadius=400)
+    
+    if circles is not None:
+        circles = numpy.uint16(numpy.around(circles))
+        chosen = None
+        for i in circles[0, :]:
+            if chosen is None: chosen = i
+            if prevCircle is not None:
+                if distance(chosen[0], chosen[1], prevCircle[0], prevCircle[1]) <= distance(i[0], i[1], prevCircle[0], prevCircle[1]):
+                    chosen = i
+
+        # cv2.circle(frame, (chosen[0], chosen[1]), 1, (0, 100, 100), 3)
+        cv2.circle(frame, (chosen[0], chosen[1]), chosen[2], (255,0,255), 3)
+        prevCircle = chosen
+
+    cv2.imshow(" ", frame)
+    # cv2.imshow("frame", blur_frame)
     # cv2.imshow("frame", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'): break
     # cv2.waitKey(1)
